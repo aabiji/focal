790b5bafad2628d35dbfac56170abdb54f2a61c9
Abigail Adegbiji <adegbijiabigail@gmail.com>
2024-06-24 20:38:03 -0600

    basic ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,77 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import TodoCard from "./TodoCard.svelte";
+</script>
+
+<div class="left-side">
+    <p> Work session </p>
+    <h1> 00:00:00 </h1>
+    <div class="controls">
+        <button>
+            <img src="/assets/play.svg" alt="play icon">
+        </button>
+        <button>
+            <img src="/assets/settings.svg" alt="settings icon">
+        </button>
+    </div>
+</div>
+
+<div class="right-side">
+    <TodoCard />
+</div>
+
+<style>
+    :global(body) {
+        width: 100%;
+        height: 100%;
+        padding: 0px;
+        margin: 0px;
+
+        background: linear-gradient(to right, #e55d87, #5fc3e4);
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .left-side {
+        width: 50%;
+        height: 100%;
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .left-side h1 {
+        font-size: 50px;
+        color: white;
+    }
+
+    .left-side p {
+        margin-bottom: -20px;
+        font-size: 20px;
+        color: white;
+    }
+
+    .controls button {
+        cursor: pointer;
+        background: none;
+        outline: none;
+        border: none;
+    }
+
+    .controls button img {
+        width: 20px;
+        height: 20px;
+    }
+
+    .right-side {
+        left: 50%;
+        width: 48.5%;
+        height: 98.5%;
+        padding: 5px 10px;
+        position: absolute;
+        background-color: rgba(255, 255, 255, 0.1);
+    }
+</style>
---
+++ src/routes/TodoCard.svelte
@@ -0,0 +1,47 @@
+
+<div class="container">
+    <div class="info">
+        <p> Your tasks </p>
+        <div class="controls">
+            <button class="add"> + </button>
+            <button class="remove"> - </button>
+        </div>
+    </div>
+</div>
+
+<style>
+    .container {
+        width: 98%;
+        height: fit-content;
+        padding: 2px 10px;
+        font-size: 16px;
+        margin-bottom: 10px;
+        background-color: rgba(255, 255, 255, 0.8);
+        border-radius: 10px;
+        cursor: pointer;
+    }
+
+    .info {
+        display: flex;
+        align-items: center;
+    }
+
+    .controls {
+        margin-left: auto;
+    }
+
+    .add {
+        color: green;
+    }
+
+    .remove {
+        color: red;
+    }
+
+    button {
+        border: none;
+        outline: none;
+        background-color: rgba(0, 0, 0, 0);
+        font-size: 25px;
+    }
+</style>
